79ab2b81bf5e8a6576c911233db68ea263dee853
Treasure-Praise <mac@Treasure-Praises-MacBook-Pro.local>
2022-09-22 23:05:46 +0100

    Persisted Navbar component across all pages using <Outlet/>

--- src/App.js
+++ src/App.js
@@ -1,37 +1,25 @@
+import { Routes,Route} from "react-router-dom";
+import Home from "./routes/home/Home.component";
+import Navigation from "./routes/navigation/Navigation.component";
 
-import CategoryContainer from './components/category-container/Category-container.component';
 
 const App = () => {
-    const categories =[
-      {
-        "id": 1,
-        "title": "hats",
-        "imageUrl": "https://i.ibb.co/cvpntL1/hats.png"
-      },
-      {
-        "id": 2,
-        "title": "jackets",
-        "imageUrl": "https://i.ibb.co/px2tCc3/jackets.png"
-      },
-      {
-        "id": 3,
-        "title": "sneakers",
-        "imageUrl": "https://i.ibb.co/0jqHpnp/sneakers.png"
-      },
-      {
-        "id": 4,
-        "title": "womens",
-        "imageUrl": "https://i.ibb.co/GCCdy8t/womens.png"
-      },
-      {
-        "id": 5,
-        "title": "mens",
-        "imageUrl": "https://i.ibb.co/R70vBrQ/men.png"
-      }
-    ] 
 
+  
+
+  const Shop=()=>{
+    return(
+      <div>I am the Shop</div>
+    )
+  }
+    
   return (
-    <CategoryContainer categories={categories}/>
+    <Routes>
+      <Route path="/" element={<Navigation/>}>
+         <Route index={true} element={ <Home/>} />
+        <Route path="shop" element={<Shop/>}/>
+      </Route>
+    </Routes>
   );
 };
 
--- src/components/category-container/Category-container.component.jsx
+++ src/components/category-container/Category-container.component.jsx
@@ -1,20 +1,14 @@
-import CategoryItem from "../category-item/Category-item.component"
-import "./Category-container.styles.scss"
+import CategoryItem from "../category-item/Category-item.component";
+import "./Category-container.styles.scss";
 
-
-const CategoryContainer=({categories})=>{
-    return(
-        <div className='categories-container'>
-      {categories.map((category)=>{
-        return(
-          <CategoryItem  key={category.id} category={category}/>
-
-        )
+const CategoryContainer = ({ categories }) => {
+  return (
+    <div className="categories-container">
+      {categories.map((category) => {
+        return <CategoryItem key={category.id} category={category} />;
       })}
-      
-    
     </div>
-    )
-}
+  );
+};
 
-export default CategoryContainer
+export default CategoryContainer;

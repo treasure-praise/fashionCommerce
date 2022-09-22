4cd46b90ee324b4df85f42e85ef98a6902fd82fc
Treasure-Praise <mac@Treasure-Praises-MacBook-Pro.local>
2022-09-22 21:13:28 +0100

    updating Category list page with category-container component and category list component

---
+++ src/components/category-container/Category-container.component.jsx
@@ -0,0 +1,20 @@
+import CategoryItem from "../category-item/Category-item.component"
+import "./Category-container.styles.scss"
+
+
+const CategoryContainer=({categories})=>{
+    return(
+        <div className='categories-container'>
+      {categories.map((category)=>{
+        return(
+          <CategoryItem  key={category.id} category={category}/>
+
+        )
+      })}
+      
+    
+    </div>
+    )
+}
+
+export default CategoryContainer
